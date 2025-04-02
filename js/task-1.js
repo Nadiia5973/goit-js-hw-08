7eaad57766f7c113e95617343acb02d3e1161115
Derkach Nadiia <derkachnadiia1@gmail.com>
2025-04-02 15:56:58 +0300

    add all files

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categ = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categ.length}`);
-categ.forEach(cat => {
-  const nameItem = cat.querySelector('h2').textContent;
-  const lengthItem = cat.querySelectorAll('li');
-  console.log(`Category: ${nameItem}`);
-  console.log(`Elements: ${lengthItem.length}`);
-});
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputText = document.querySelector('#name-input');
-const spanAnon = document.querySelector('#name-output');
-inputText.addEventListener('input', event => {
-  const inputValue = event.target.value.trim();
-  spanAnon.textContent === ''
-    ? (spanAnon.textContent = 'Anonymous')
-    : (spanAnon.textContent = inputValue);
-});
